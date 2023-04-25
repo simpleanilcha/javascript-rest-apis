6ccd7c18f2659606307bec7c82c62962816cb90a
Anil Maharjan <simpleanilcha@gmail.com>
2023-04-26 04:03:51 +0545

    Get a single piece of data

--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -11,6 +11,16 @@ const pieRepo = {
         resolve(JSON.parse(data))
       }
     })
+  },
+  getById: function (id, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        let pie = JSON.parse(data).find(p => p.id == id)
+        resolve(pie)
+      }
+    })
   }
 }
 
