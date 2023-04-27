2bf486e958d1894f159b4c4eba8b1fffcf76d47c
Anil Maharjan <simpleanilcha@gmail.com>
2023-04-27 22:25:09 +0545

    Create PATCH endpoint to patch data

--- index.js
+++ index.js
@@ -152,6 +152,33 @@ router.delete('/:id', function (req, res, next) {
   })
 })
 
+// PATCH
+router.patch('/:id', function (req, res, next) {
+  pieRepo.getById(req.params.id, function (data) {
+    if (data) {
+      // Attempt to update the data
+      pieRepo.update(req.body, req.params.id, function (data) {
+        res.status(200).json({
+          'status': 200,
+          'statusText': 'OK',
+          'message': "Pie '" + req.params.id + "' patched.",
+          'data': data
+        })
+      })
+    } else {
+      res.status(404).json({
+        'status': 404,
+        'statusText': 'Not Found',
+        'message': "Pie '" + req.params.id + "' could not be found.",
+        'error': {
+          'code': 'NOT_FOUND',
+          'message': "Pie '" + req.params.id + "' could not be found."
+        }
+      })
+    }
+  })
+})
+
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router)
 
