630909722575fd98bec10c891428ffd9ef2eed30
Anil Maharjan <simpleanilcha@gmail.com>
2023-04-29 19:30:08 +0545

    Add Request package to website and call REST APIs

--- BethanysPiesWebsite/routes/index.js
+++ BethanysPiesWebsite/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const apiHelper = require('../helpers/apiHelpers')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: "Bethany's Pie Shop" });
+  apiHelper.callApi('http://localhost:5000/api')
+    .then(response => {
+      console.log(response)
+      res.render('index',
+        { title: "Bethany's Pie Shop", data: response.data }
+      )
+    })
+    .catch(error => {
+      res.send(error)
+    })
 });
 
 module.exports = router;
